a85fb9c92c0e03b1969ab373c118b3ff548333ff
Savio Alexandre <alexandresaviosouza@gmail.com>
2026-09-23 17:55:44 -0300

    feat: adiciona arquivos aula 38 introdução POM

---
+++ aula38-intro-pom.js
@@ -0,0 +1,34 @@
+// ========================================
+// JEITO RUIM (Sem Page Object)
+// ========================================
+
+// Imagine isso repetido em 10 arquivos de teste diferentes...
+function testeLoginRuim() {
+    console.log("Preenchendo #user com 'joao'");
+    console.log("Preenchendo #pass com '123'");
+    console.log("Clicando em #btn-submit");
+}
+
+// ========================================
+// JEITO BOM (Com Page Object)
+// ========================================
+
+// 1. Criamos o "Mapa" da página (o Page Object)
+const loginPage = {
+    // Elementos (Seletores)
+    inputUsuario: "#user",
+    inputSenha: "#pass",
+    botaoEntrar: "#btn-submit",
+
+    // Ações (Métodos)
+    logar: function(usuario, senha) {
+        console.log(`Preenchendo '${this.inputUsuario}' com '${usuario}'`);
+        console.log(`Preenchendo '${this.inputSenha}' com '${senha}'`);
+        console.log(`Clicando em '${this.botaoEntrar}'`);
+    }
+};
+
+//O teste fica limpo e legivel
+function testeLoginBom(){
+    loginPage.logar("João", "123");
+}
